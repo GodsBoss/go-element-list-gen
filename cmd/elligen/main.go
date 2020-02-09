fc72ea11287bdc766c13e4ed7bf23715497f8475
Timo Reitz <godsboss@gmx.de>
2020-02-09 15:57:22 +0100

    Expand main() a bit.

--- cmd/elligen/main.go
+++ cmd/elligen/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(os.Args[1:]); err != nil {
+		fmt.Printf("Error: %+v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
+	return nil
+}
