04b8003f315660f2f1c027ba6bfe704eff9dabf9
Timo Reitz <godsboss@gmx.de>
2020-02-09 16:34:31 +0100

    Make consonants, vowels and endings customizable via command line.

--- cmd/elligen/main.go
+++ cmd/elligen/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	elligen "github.com/GodsBoss/go-element-list-gen"
 )
@@ -17,16 +18,36 @@ func main() {
 
 func run(args []string) error {
 	fs := flag.NewFlagSet("elements name generator", flag.ContinueOnError)
+
 	attempts := fs.Int("attempts", 0, "Number of failed attempts when generating names.")
 	count := fs.Int("count", 10, "Number of names to be generated.")
+
+	consonants := fs.String("consonants", "", "Comma-separated list of consonants. A default list is used if this is empty.")
+	vowels := fs.String("vowels", "", "Comma-separated list of vowels. A default list is used if this is empty.")
+	endings := fs.String("endings", "", "Comma-separated list of endings. A default list is used if this is empty.")
+
 	err := fs.Parse(args)
 	if err != nil {
 		return err
 	}
-	generator, err := elligen.DefaultGenerator()
+
+	options := make([]elligen.DefaultGeneratorOption, 0)
+
+	if len(*consonants) > 0 {
+		options = append(options, elligen.WithConsonants(strings.Split(*consonants, ",")))
+	}
+	if len(*vowels) > 0 {
+		options = append(options, elligen.WithVowels(strings.Split(*vowels, ",")))
+	}
+	if len(*endings) > 0 {
+		options = append(options, elligen.WithEndings(strings.Split(*endings, ",")))
+	}
+
+	generator, err := elligen.DefaultGenerator(options...)
 	if err != nil {
 		return err
 	}
+
 	list, err := elligen.Generate(generator, *count, *attempts)
 	if len(list) > 0 {
 		fmt.Println("Generated names:")
