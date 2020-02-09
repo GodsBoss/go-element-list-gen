97e5fc8ccefb80cd7452cf63999b201b54710a41
Timo Reitz <godsboss@gmx.de>
2020-02-09 16:10:22 +0100

    Actually generate names.

--- cmd/elligen/main.go
+++ cmd/elligen/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+
+	"github.com/GodsBoss/elligen"
 )
 
 func main() {
@@ -13,5 +16,23 @@ func main() {
 }
 
 func run(args []string) error {
-	return nil
+	fs := flag.NewFlagSet("elements name generator", flag.ContinueOnError)
+	attempts := fs.Int("attempts", 0, "Number of failed attempts when generating names.")
+	count := fs.Int("count", 10, "Number of names to be generated.")
+	err := fs.Parse(args)
+	if err != nil {
+		return err
+	}
+	generator, err := elligen.DefaultGenerator()
+	if err != nil {
+		return err
+	}
+	list, err := elligen.Generate(generator, *count, *attempts)
+	if len(list) > 0 {
+		fmt.Println("Generated names:")
+		for i := range list {
+			fmt.Printf("- %s\n", list[i])
+		}
+	}
+	return err
 }
